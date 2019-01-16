7f2da60aeb0ec50cc45397f0b1ce65a756912f0d
hezhanwei111 <2723095643@qq.com>
2019-01-16 09:47:00 +0800

    前台页面

---
+++ application/home/model/Type.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace app\home\model;
+
+use think\Model;
+
+class Type extends Model
+{
+   //数据表名
+   protected $table = 'type';
+   //表主键
+   protected $pk = 'id';
+
+   public function getTypenameAttr(){
+   	$n = substr_count($this->path,',')-1;
+   	$space = str_repeat('&nbsp',$n*6);
+   	$name = $space.$this['name'];
+   	return $name;
+   }
+}
---
+++ application/home/model/User.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\home\model;
+
+use think\Model;
+
+class User extends Model
+{
+    //数据表名
+    protected $table = 'User';
+    //表主键
+    protected $pk = 'id';
+
+}
